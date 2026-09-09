e0b041283be80617d39b41b7e0bc7c51ab0ead80
duyuanjin2009-blip <duyuanjin2009@gmail.com>
2026-09-09 18:15:07 +0800

    fix(android): align Java and Kotlin targets

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,11 @@ plugins {
 
 android { namespace = "cc.duyuan.hkvpn"; compileSdk = 35
     defaultConfig { applicationId = "cc.duyuan.hkvpn"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "1.0.0" }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
 }
 
 dependencies {
